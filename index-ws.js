c748dcdf5f813bc0a21eee4e037a279515ad210d
bhargav mungara <bhargavmungara@bhargavs-MacBook-Air.local>
2025-12-14 18:05:05 +0530

    Add database logging

--- index-ws.js
+++ index-ws.js
@@ -9,6 +9,15 @@ app.get('/',function(req,res){
 server.on('request',app)
 server.listen(3000,function(){console.log('server started on port 3000!')})
 
+process.on('SIGINT', () => {
+    console.log('sigint');
+    wss.clients.forEach(function each(client) {
+        client.close();
+    });
+    server.close(() => {
+        shutDownDB();
+    })
+})
 
 // Begin WebSockets
 const WebSocketServer = require('ws').Server;
@@ -25,6 +34,10 @@ wss.on('connection',function connection(ws){
         ws.send('Welcome to my server')
     }
 
+    db.run(`INSERT INTO visitors (count, time)
+        VALUES (${numClients}, datetime('now'))    
+    `)
+
     ws.on('close',function close(){
         wss.broadcast(`Current visitors: ${numClients}`)
         console.log('A client has disconnected')
@@ -39,3 +52,28 @@ wss.broadcast=function broadcast(data){
     })
 }
 
+// end websockets and begin database
+
+const sqlite=require('sqlite3')
+const db=new sqlite.Database(':memory:')
+
+db.serialize(()=>{
+    db.run(`
+        CREATE TABLE visitors(
+            count INTEGER,
+            time TEXT
+        )    
+    `)
+})
+
+function getCounts(){
+    db.each("SELECT * FROM visitors",(err,row)=>{
+        console.log(row)
+    })
+}
+
+function shutDownDB(){
+    getCounts()
+    console.log('Shutting down the DB')
+    db.close()
+}
